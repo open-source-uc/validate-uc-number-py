6ded510fa9c694e8a836302131157604859d40b1
Patricio López <lopezjuripatricio@gmail.com>
2015-12-13 13:19:37 -0300

    add setup settings

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='uc-numero-alumno',
+      version='0.1.0',
+      description='Valida un número de alumno de la UC ',
+      url='https://github.com/mrpatiwi/uc-numero-alumno-python',
+      author='Patricio López',
+      author_email='patricio@lopezjuri.com',
+      license='MIT',
+      packages=['ucnumber'],
+      zip_safe=False)
