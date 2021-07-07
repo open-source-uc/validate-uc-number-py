7d0ad9e9ee72cf3323aad6397c4612332f0ea518
Agustín Covarrubias <agucova@uc.cl>
2021-07-06 23:24:35 -0400

    fix: bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='ucnumber',
-      version='0.1.0',
+      version='0.1.1',
       description='Validate UC alumni identifier numbers.',
       url='https://github.com/mrpatiwi/uc-numero-alumno-python',
       author='Patricio López',
